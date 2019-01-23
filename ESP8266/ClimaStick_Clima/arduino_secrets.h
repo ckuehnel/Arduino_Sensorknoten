f642976c65ee56fa463eaef205bb04a44e7ef861
Claus Kühnel <info@ckuehnel.ch>
2019-01-23 14:27:31 +0100

    Update arduino_secrets.h

--- ESP8266/ClimaStick_Clima/arduino_secrets.h
+++ ESP8266/ClimaStick_Clima/arduino_secrets.h
@@ -2,9 +2,9 @@
 
 // Wifi 
 #define mySSID "Sunrise_2.4GHz_8AC2A0"
-#define myPW "u2u7fgzv31Ds"
+#define myPW "xxxxxxx"
 
 // Thinger.io
 #define USERNAME "ckuehnel"
 #define DEVICE_ID "ClimaStick"
-#define DEVICE_CREDENTIAL "climastick"
+#define DEVICE_CREDENTIAL "xxxxxx"
