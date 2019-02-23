fde2786caa4a99a2f128e2ffad90d5af2999e31d
Claus Kühnel <info@ckuehnel.ch>
2019-02-23 11:21:27 +0100

    Add files via upload

---
+++ ClimaStick/arduino_secrets.h
@@ -0,0 +1,10 @@
+// This file contains credentials to access several services
+
+// Wifi 
+#define mySSID "Sunrise_2.4GHz_8AC2A0"
+#define myPW "u2u7fgzv31Ds"
+
+// Thinger.io
+#define USERNAME "ckuehnel"
+#define DEVICE_ID "ClimaStick"
+#define DEVICE_CREDENTIAL "climastick"
